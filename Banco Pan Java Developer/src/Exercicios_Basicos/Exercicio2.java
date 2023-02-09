60dffbe9bc955de96d593ddfd639601a149ccc34
Julia Farias da Rocha <jufarias20002@gmail.com>
2023-02-08 23:05:30 -0300

    update

---
+++ Banco Pan Java Developer/src/Exercicios_Basicos/Exercicio2.java
@@ -0,0 +1,10 @@
+package Exercicios_Basicos;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Banco Pan Java Developer/src/Exercicios_Basicos/Exercicio3.java
@@ -0,0 +1,10 @@
+package Exercicios_Basicos;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
