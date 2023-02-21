bab56f28b5417aeefc655de060e7c3f878ca0345
julia <jufarias20002@gmail.com>
2023-02-21 13:00:00 -0300

    creating the project structure

---
+++ yu-gi-oh/src/models/Cards.ts
@@ -0,0 +1,13 @@
+interface Cards{
+    id:number,
+    name:string,
+    type:string,
+    description:string,
+    atk:number,
+    def:number,
+    level:number,
+    images:{
+        id:number,
+        image:string,
+    }[],
+}
